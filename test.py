9b717afa4b7d858fd90c667d308db08892e370e8
Rob <rob.guderian@gmail.com>
2018-01-22 11:01:52 -0600

    More unit tests, Unit test config file.

--- test.py
+++ test.py
@@ -4,11 +4,19 @@ from userStatusBot import checkIsPersonTeaching, checkWhenIsPersonTeaching
 class TestRegex(unittest.TestCase):
     # does a lower(), so all test have to check the lower-casing
 
+    def setUp(self):
+        f = open("teaching_status_config.json.unittest")
+        self.config = json.load(f)
+
     def test_isTeaching(self):
         name = checkIsPersonTeaching("Is Robert teaching")
         self.assertEqual(name, "robert")
         name = checkIsPersonTeaching("Is Robert teaching?")
         self.assertEqual(name, "robert")
+
+        name = checkIsPersonTeaching("Is Robert G teaching?")
+        self.assertEqual(name, "robert g")
+
 
         # consideration for future releases
         name = checkIsPersonTeaching("do you think that Robert teaching?")
@@ -19,6 +27,9 @@ class TestRegex(unittest.TestCase):
         self.assertEqual(name, "robert")
         name = checkIsPersonTeaching("When Is Robert teaching?")
         self.assertEqual(name, "robert")
+        name = checkIsPersonTeaching("When Is Robert g teaching?")
+        self.assertEqual(name, "robert g")
+
 
 
 if __name__ == '__main__':
--- userStatusBot.py
+++ userStatusBot.py
@@ -2,6 +2,7 @@ import os
 import time
 import re
 import json
+from datetime import datetime
 
 from slackclient import SlackClient
 
@@ -10,6 +11,18 @@ compiledIPT = re.compile(isPersonTeaching)
 
 whenIsPersonTeaching = "when\s+is\s+(?P<name>.+)\s+teaching"
 compiledWIPT = re.compile(whenIsPersonTeaching)
+
+def findUser(name, config):
+    """
+    Find a user based on a name.
+    The name can be a first name, last name, both.
+    Fuzzy match, score the names to the users. Return best matches.
+    """
+    # check perfect firstname/lastname matches
+    # check perfect lastname matches
+    # check perfect firstname matches
+    # use hamming distance to fuzzy match.
+    pass
 
 def checkIsPersonTeaching(msg):
     matches = compiledIPT.search(msg.lower())
@@ -23,13 +36,21 @@ def checkWhenIsPersonTeaching(msg):
         name = matches.group('name')
         return name
 
-def getWhenIsPersonTeaching(name):
+def getWhenIsPersonTeaching(name, config):
     pass
 
-def getIsPersonTeaching(name):
-    pass
+def getIsPersonTeaching(name, config):
+    """
+    Check if a user is teaching. Return strings about users that matched
+    the name filter.
+    returns an array of strings.
+    """
+    # find user in file
+    users = findUsers(name, config)
+    currtime = datetime.now()
+    milTime = currtime.hour * 100 + currtime.minute
 
-def parse_bot_commands(slack_events):
+def parse_bot_commands(slack_events, config):
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
             message = event["text"]
@@ -37,11 +58,11 @@ def parse_bot_commands(slack_events):
             # note: order here is important
             name = checkWhenIsPersonTeaching(message)
             if name is not None:
-                getWhenIsPersonTeaching(name)
+                getWhenIsPersonTeaching(name, config)
             else:
                 name = checkIsPersonTeaching(message)
                 if name is not None:
-                    getIsPersonTeaching(name)
+                    getIsPersonTeaching(name, config)
 
 if __name__ == "__main__":
     # read config file
@@ -61,7 +82,7 @@ if __name__ == "__main__":
         # Read bot's user ID by calling Web API method `auth.test`
         userStatusbot_id = slack_client.api_call("auth.test")["user_id"]
         while True:
-            parse_bot_commands(slack_client.rtm_read())
+            parse_bot_commands(slack_client.rtm_read(), config)
             time.sleep(RTM_READ_DELAY)
     else:
         print("Connection failed. Exception traceback printed above.")
